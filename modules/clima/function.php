e46d45218cf5db2a42a756354ac3f15a539b6384
eduardoabach <eduardoabach@gmail.com>
2017-06-28 17:49:56 -0300

    Clima usando formato api

--- modules/clima/function.php
+++ modules/clima/function.php
@@ -1,52 +1,5 @@
 <?php 
 
-// Siglas das condições do tempo do INPE
-function get_inpe_cond_tempo($sigla){
-	$list = array(
-		'ec'=>array('nome'=>'Encoberto com Chuvas Isoladas', 'descricao'=>'Céu totalmente encoberto com chuvas em algumas regiões, sem aberturas de sol.'),
-		'ci'=>array('nome'=>'Chuvas Isoladas', 'descricao'=>'Muitas nuvens com curtos períodos de sol e chuvas em algumas áreas.'),
-		'c'=>array('nome'=>'Chuva', 'descricao'=>'Muitas nuvens e chuvas periódicas.'),
-		'in'=>array('nome'=>'Instável', 'descricao'=>'Nebulosidade variável com chuva a qualquer hora do dia.'),
-		'pp'=>array('nome'=>'Poss. de Pancadas de Chuva', 'descricao'=>'Nebulosidade variável com pequena chance (inferior a 30%) de pancada de chuva.'),
-		'cm'=>array('nome'=>'Chuva pela Manhã', 'descricao'=>'Chuva pela manhã melhorando ao longo do dia.'),
-		'cn'=>array('nome'=>'Chuva a Noite', 'descricao'=>'Nebulosidade em aumento e chuvas durante a noite.'),
-		'pt'=>array('nome'=>'Pancadas de Chuva a Tarde', 'descricao'=>'Predomínio de sol pela manhã. À tarde chove com trovoada.'),
-		'pm'=>array('nome'=>'Pancadas de Chuva pela Manhã', 'descricao'=>'Chuva com trovoada pela manhã. À tarde o tempo abre e não chove.'),
-		'np'=>array('nome'=>'Nublado e Pancadas de Chuva', 'descricao'=>'Muitas nuvens com curtos períodos de sol e pancadas de chuva com trovoadas.'),
-		'pc'=>array('nome'=>'Pancadas de Chuva', 'descricao'=>'Chuva de curta duração e pode ser acompanhada de trovoadas a qualquer hora do dia.'),
-		'pn'=>array('nome'=>'Parcialmente Nublado', 'descricao'=>'Sol entre poucas nuvens.'),
-		'cv'=>array('nome'=>'Chuvisco', 'descricao'=>'Muitas nuvens e chuva fraca composta de pequenas gotas d´ água.'),
-		'ch'=>array('nome'=>'Chuvoso', 'descricao'=>'Nublado com chuvas contínuas ao longo do dia.'),
-		't'=>array('nome'=>'Tempestade', 'descricao'=>'Chuva forte capaz de gerar granizo e ou rajada de vento, com força destrutiva (Veloc. aprox. de 90 Km/h) e ou tornados.'),
-		'ps'=>array('nome'=>'Predomínio de Sol', 'descricao'=>'Sol na maior parte do período.'),
-		'e'=>array('nome'=>'Encoberto', 'descricao'=>'Céu totalmente encoberto, sem aberturas de sol.'),
-		'n'=>array('nome'=>'Nublado', 'descricao'=>'Muitas nuvens com curtos períodos de sol.'),
-		'cl'=>array('nome'=>'Céu Claro', 'descricao'=>'Sol durante todo o período. Ausência de nuvens.'),
-		'nv'=>array('nome'=>'Nevoeiro', 'descricao'=>'Gotículas de água em suspensão que reduzem a visibilidade.'),
-		'g'=>array('nome'=>'Geada', 'descricao'=>'Cobertura de cristais de gelo que se formam por sublimação direta sobre superfícies expostas cuja temperatura está abaixo do ponto de congelamento.'),
-		'ne'=>array('nome'=>'Neve', 'descricao'=>'Vapor de água congelado na nuvem, que cai em forma de cristais e flocos.'),
-		'nd'=>array('nome'=>'Não Definido', 'descricao'=>'Não definido.'),
-		'pnt'=>array('nome'=>'Pancadas de Chuva a Noite', 'descricao'=>'Chuva de curta duração podendo ser acompanhada de trovoadas à noite.'),
-		'psc'=>array('nome'=>'Possibilidade de Chuva', 'descricao'=>'Nebulosidade variável com pequena chance (inferior a 30%) de chuva.'),
-		'pcm'=>array('nome'=>'Possibilidade de Chuva pela Manhã', 'descricao'=>'Nebulosidade variável com pequena chance (inferior a 30%) de chuva pela manhã.'),
-		'pct'=>array('nome'=>'Possibilidade de Chuva a Tarde', 'descricao'=>'Nebulosidade variável com pequena chance (inferior a 30%) de chuva pela tarde.'),
-		'pcn'=>array('nome'=>'Possibilidade de Chuva a Noite', 'descricao'=>'Nebulosidade variável com pequena chance (inferior a 30%) de chuva à noite.'),
-		'npt'=>array('nome'=>'Nublado com Pancadas a Tarde', 'descricao'=>'Muitas nuvens com curtos períodos de sol e pancadas de chuva com trovoadas à tarde.'),
-		'npn'=>array('nome'=>'Nublado com Pancadas a Noite', 'descricao'=>'Muitas nuvens com curtos períodos de sol e pancadas de chuva com trovoadas à noite.'),
-		'ncn'=>array('nome'=>'Nublado com Poss. de Chuva a Noite', 'descricao'=>'Muitas nuvens com curtos períodos de sol com pequena chance (inferior a 30%) de chuva à noite.'),
-		'nct'=>array('nome'=>'Nublado com Poss. de Chuva a Tarde', 'descricao'=>'Muitas nuvens com curtos períodos de sol com pequena chance (inferior a 30%) de chuva à tarde.'),
-		'ncm'=>array('nome'=>'Nubl. c/ Poss. de Chuva pela Manhã', 'descricao'=>'Muitas nuvens com curtos períodos de sol com pequena chance (inferior a 30%) de chuva pela manhã.'),
-		'npm'=>array('nome'=>'Nublado com Pancadas pela Manhã', 'descricao'=>'Muitas nuvens com curtos períodos de sol e chuva com trovoadas pela manhã.'),
-		'npp'=>array('nome'=>'Nublado com Possibilidade de Chuva', 'descricao'=>'Muitas nuvens com curtos períodos de sol com pequena chance (inferior a 30%) de chuva a qualquer hora do dia.'),
-		'vn'=>array('nome'=>'Variação de Nebulosidade', 'descricao'=>'Períodos curtos de sol intercalados com períodos de nuvens.'),
-		'ct'=>array('nome'=>'Chuva a Tarde', 'descricao'=>'Nebulosidade em aumento e chuvas a partir da tarde.'),
-		'ppn'=>array('nome'=>'Poss. de Panc. de Chuva a Noite', 'descricao'=>'Nebulosidade variável com pequena chance (inferior a 30%) de chuva à noite.'),
-		'ppt'=>array('nome'=>'Poss. de Panc. de Chuva a Tarde', 'descricao'=>'Nebulosidade variável com pequena chance (inferior a 30%) de chuva pela tarde.'),
-		'ppm'=>array('nome'=>'Poss. de Panc. de Chuva pela Manhã', 'descricao'=>'Nebulosidade variável com pequena chance (inferior a 30%) de chuva pela manhã.'));
-
-	return (isset($list[$sigla])) ? $list[$sigla] : false;
-}
-
 function get_inpe_img_clima_sigla($sigla){
 	return 'modules/clima/img/inpe/'.$sigla.'.png';
 }
--- modules/clima/prev_clima.php
+++ modules/clima/prev_clima.php
@@ -10,6 +10,38 @@ if(!($idCidade > 0)){
 	return false;
 }
 
+$dataRef = date('Y-m-d');
+$qtdDiasVerif = 7;
+$listDias = array();
+for($i=0;$i<$qtdDiasVerif;$i++){
+	if($i > 0)
+		$dataRef = add_days($dataRef,1);
+	$listDias[$dataRef]['dia'] = $dataRef;
+}
+
+sys_load_md_api('inpe_webservice');
+$inpeServ = new inpe_webservice();
+$previsaoTempo = $inpeServ->get_previsao_tempo_cidade($idCidade);
+
+if(isset($previsaoTempo['dias'])){
+	$listDias = array_merge($listDias, $previsaoTempo['dias']);
+}
+
+// pr($listDias);
+// die('a');
+
+// $infoUv = get_oms_indice_uv(trim($inf['iuv']));
+// $corUv = $infoUv['cor'];
+// $corTempMin = get_cor_temperatura($diaPrev->minima);
+// $corTempMax = get_cor_temperatura($diaPrev->maxima);
+
+//get_dia_da_semana($diaPrev->dia, 3)
+//$infoUv['risco']
+//$infoUv['cor']
+//$diaPrev->iuv
+//get_inpe_img_clima_sigla(trim($diaPrev->tempo))
+
+
 //ver
 // http://www.flextool.com.br/tabela_cores.html
 // http://astropbi.blogspot.com.br/2009/09/fases-da-lua.html
@@ -54,61 +86,55 @@ $xmlOb = new SimpleXMLElement($page);
 	    	</thead>
 	    	<tbody>
 	    		<?php
-	    		if(isset($xmlOb->previsao)){
-					foreach($xmlOb->previsao as $diaPrev){
-						$siglaDesc = get_inpe_cond_tempo(trim($diaPrev->tempo));
-						$clima = ($siglaDesc !== false) ? $siglaDesc : '';
-						$infoUv = get_oms_indice_uv(trim($diaPrev->iuv));
+	    		foreach($listDias as $diaInf){
 
-						$dataUser = date_to_user($diaPrev->dia);
-						$dataArr = data_to_array($diaPrev->dia);
-						$nomeMes = get_nome_mes_ano($dataArr['mes']);
+					$dataUser = date_to_user($diaInf['dia']);
+					$dataArr = data_to_array($diaInf['dia']);
+					$nomeMes = get_nome_mes_ano($dataArr['mes']);
 
-						// LUA 
-						$infoFasesLua = fases_lua($diaPrev->dia);
-						$faseDaLua = round($infoFasesLua['fracional'], 4);
-						$descLua = $infoFasesLua['descricao_completa'];
+					//ajustar a lua para usar o astronomico apenas, func para monstar desc, conceitos de fases em ptbr tbm
+					//passar a usar dados do astronomico
 
-						//ajustar a lua para usar o astronomico apenas, func para monstar desc, conceitos de fases em ptbr tbm
-						//passar a usar dados do astronomico
+					// ASTRONOMICO, Lua / Sol
+					$infoAstr = get_astronomico(trim($diaInf['dia']));
+					$faseDaLuaAstr = round($infoAstr['lua']['fase'], 4);
+					$descLuaAstr = $infoAstr['lua']['fase'];
 
-						// ASTRONOMICO
-						$infoAstr = get_astronomico(trim($diaPrev->dia));
-						$faseDaLuaAstr = round($infoAstr['lua']['fase'], 4);
-						$descLuaAstr = $infoAstr['lua']['fase'];
+					// IUV
+	    			$infoUv = get_oms_indice_uv(trim($diaInf['iuv']));
+					$corUv = $infoUv['cor'];
 
-						$corUv = $infoUv['cor'];
-						$corTempMin = get_cor_temperatura($diaPrev->minima);
-						$corTempMax = get_cor_temperatura($diaPrev->maxima);
-						?>
-						<tr>
-							<td align="center" title="<?=$dataUser?>">
-								<button type="button" class="btn btn-sm btn-default info-dia" data-dia="<?=trim($diaPrev->dia)?>">
-								<?=$dataArr['dia']?><br>
-								<?=$nomeMes?>	
-								</button> 
-							</td>
-							<td><?=get_dia_da_semana($diaPrev->dia, 3)?></td>
-							<td align="center">
-								<strong style="color:<?=$corTempMin?>"><?=$diaPrev->minima?></strong>~
-								<strong style="color:<?=$corTempMax?>"><?=$diaPrev->maxima?></strong>
-							</td>
-							<td data-descricao="<?=$clima['descricao']?>">
-								<?=$clima['nome']?>		
-							</td>
-							<td align="center">
-								<img width="55" height="37" src='<?=get_inpe_img_clima_sigla(trim($diaPrev->tempo))?>'>
-							</td>
-							<td align="center">
-								<strong title="RISCO <?=$infoUv['risco']?>" style="color:<?=$infoUv['cor']?>"><?=(int)$diaPrev->iuv?></strong>
-							</td>
-							<td align="center">
-								<div class="lua-fase" data-fase="<?=$faseDaLuaAstr?>" data-size="30" title="<?=$descLua?>"></div>
-							</td>
-						</tr>
-						<?php
-					}
-				}
+					// TEMPERATURA
+					$corTempMin = get_cor_temperatura($diaInf['minima']);
+					$corTempMax = get_cor_temperatura($diaInf['maxima']);
+					?>
+					<tr>
+						<td align="center" title="<?=$dataUser?>">
+							<button type="button" class="btn btn-sm btn-default info-dia" data-dia="<?=trim($diaInf['dia'])?>">
+							<?=$dataArr['dia']?><br>
+							<?=$nomeMes?>	
+							</button> 
+						</td>
+						<td><?=get_dia_da_semana($diaInf['dia'], 3)?></td>
+						<td align="center">
+							<strong style="color:<?=$corTempMin?>"><?=$diaInf['minima']?></strong>~
+							<strong style="color:<?=$corTempMax?>"><?=$diaInf['maxima']?></strong>
+						</td>
+						<td data-descricao="<?=$diaInf['tempo_inf']['descricao']?>">
+							<?=$diaInf['tempo_inf']['nome']?>		
+						</td>
+						<td align="center">
+							<img width="55" height="37" src='<?=get_inpe_img_clima_sigla(trim($diaInf['tempo']))?>'>
+						</td>
+						<td align="center">
+							<strong title="RISCO <?=$infoUv['risco']?>" style="color:<?=$infoUv['cor']?>"><?=(int)$diaInf['iuv']?></strong>
+						</td>
+						<td align="center">
+							<div class="lua-fase" data-fase="<?=$faseDaLuaAstr?>" data-size="30" title="<?=$descLuaAstr?>"></div>
+						</td>
+					</tr>
+					<?php
+	    		}
 	    		?>
 	    	</tbody>
 		</table>
