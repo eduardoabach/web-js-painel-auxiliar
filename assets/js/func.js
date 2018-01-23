2cfcaa4e78eee0932c3bce78c11c56d7ad353bcb
eduardoabach <eduardoabach@gmail.com>
2018-01-23 10:37:36 -0200

    Player melhorado e ignorando arquivos mp3

--- assets/js/func.js
+++ assets/js/func.js
@@ -62,6 +62,33 @@ function draggable(id, desativ){
       }
    });
 }
+
+function file_download(conteudo, filename) {
+   var element = document.createElement('a');
+   element.setAttribute('href', conteudo);
+   element.setAttribute('download', filename);
+
+   element.style.display = 'none';
+   document.body.appendChild(element);
+
+   element.click();
+   document.body.removeChild(element);
+}
+
+function file_download_type(conteudo, filename, contentType) {
+   if(!contentType){
+     contentType = 'plain/text'; //'image/gif', 'plain/text', 'application/octet-stream'
+   }
+   var element = document.createElement('a');
+   element.setAttribute('href', 'data:' + contentType + ';charset=utf-8,' + encodeURIComponent(conteudo));
+   element.setAttribute('download', filename);
+
+   element.style.display = 'none';
+   document.body.appendChild(element);
+
+   element.click();
+   document.body.removeChild(element);
+}
  
 document.onmouseup = function(e){
    window.drag_obj = null;
